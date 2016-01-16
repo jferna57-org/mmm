bbb73b56b5d9cf5c64335d13236493824eaaf870
juan.carlos.fernandez <juan.carlos.fernandez@ricoh.es>
2016-01-16 09:39:57 +0100

    sample inserts

---
+++ src/test/resources/sql/datos.sql
@@ -0,0 +1,174 @@
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 20879.07, '2015-01-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 21611.74, '2015-02-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 22812.85, '2015-03-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 25363.50, '2015-04-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 71221.38, '2015-05-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 72037.45, '2015-06-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 75864.72, '2015-07-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 76076.00, '2015-08-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 76436.63, '2015-09-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 78516.89, '2015-10-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 79889.66, '2015-11-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 79889.66, '2015-12-01', '', 1, 5);
+
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 44678.12, '2014-01-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 47967.34, '2014-02-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 48636.19, '2014-03-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 9646.37, '2014-04-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 26995.87, '2014-05-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 11684.40, '2014-06-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 6689.84, '2014-07-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 3456.25, '2014-08-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 20216.03, '2014-09-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 18283.65, '2014-10-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 17485.28, '2014-11-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 19497.58, '2014-12-01', '', 1, 5);
+
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 5782.44 , '2013-01-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 7312.48 , '2013-02-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 8830.69 , '2013-03-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 12501.57, '2013-04-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 28379.90, '2013-05-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 29305.36, '2013-06-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 33187.57, '2013-07-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 33955.88, '2013-08-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 34819.82, '2013-09-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 76569.68, '2013-10-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 40247.57, '2013-11-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 40727.39, '2013-12-01', '', 1, 5);
+
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 1926.60 , '2012-01-01', '', 1,5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 2679.02 , '2012-02-01', '', 1,5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 3529.04 , '2012-03-01', '', 1,5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 2171.63 , '2012-04-01', '', 1,5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 3061.31 , '2012-05-01', '', 1,5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 2727.25 , '2012-06-01', '', 1,5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 9443.28 , '2012-07-01', '', 1,5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 9234.12 , '2012-08-01', '', 1,5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 12610.87, '2012-09-01', '', 1,5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 13360.09, '2012-10-01', '', 1,5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 2413.30 , '2012-11-01', '', 1,5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 3541.75 , '2012-12-01', '', 1,5);
+
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 2117.33, '2011-01-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 3483.51, '2011-02-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 1552.88, '2011-03-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 2454.10, '2011-04-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 2763.86, '2011-05-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 3223.30, '2011-06-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 2178.51, '2011-07-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 2729.79, '2011-08-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 3863.97, '2011-09-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 4180.76, '2011-10-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 4899.60, '2011-11-01', '', 1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 6526.03, '2011-12-01', '', 1, 5);
+
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 8926.62 , '2010-01-01', '',1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 3926.62 , '2010-02-01', '',1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 5455.46 , '2010-03-01', '',1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 3340.44 , '2010-04-01', '',1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 4500.00 , '2010-05-01', '',1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 6000.00 , '2010-06-01', '',1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 1011.91 , '2010-07-01', '',1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 3083.61 , '2010-08-01', '',1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 2467.58 , '2010-09-01', '',1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 3904.17 , '2010-10-01', '',1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 4000.00 , '2010-11-01', '',1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 5284.70 , '2010-12-01', '',1, 5);
+
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 5630.00 , '2009-01-01', '',1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 3866.61 , '2009-02-01', '',1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 415.14  , '2009-03-01', '',1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 2036.06 , '2009-04-01', '',1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 2615.55 , '2009-05-01', '',1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 3759.15 , '2009-06-01', '',1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 3047.66 , '2009-07-01', '',1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 3100    , '2009-08-01', '',1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 3518.03 , '2009-09-01', '',1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 4349.68 , '2009-10-01', '',1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 4950.43 , '2009-11-01', '',1, 5);
+INSERT INTO mmm.historico_saldo ( saldo, fecha, notas, activo_id, user_id)
+VALUES( 6221.88 , '2009-12-01', '',1, 5);
