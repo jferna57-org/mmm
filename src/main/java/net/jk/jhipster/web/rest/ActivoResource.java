c39b585fee1540895c46dc397a1293eb604c3d0f
juan.carlos.fernandez <juan.carlos.fernandez@ricoh.es>
2016-01-22 19:15:22 +0100

    add some delete imports

--- src/main/java/net/jk/jhipster/web/rest/ActivoResource.java
+++ src/main/java/net/jk/jhipster/web/rest/ActivoResource.java
@@ -4,6 +4,8 @@ import com.codahale.metrics.annotation.Timed;
 import net.jk.jhipster.domain.Activo;
 import net.jk.jhipster.repository.ActivoRepository;
 import net.jk.jhipster.repository.search.ActivoSearchRepository;
+import net.jk.jhipster.security.SecurityUtils;
+import net.jk.jhipster.web.rest.dto.SaldoLastMonth;
 import net.jk.jhipster.web.rest.util.HeaderUtil;
 import net.jk.jhipster.web.rest.util.PaginationUtil;
 import org.slf4j.Logger;
@@ -20,6 +22,8 @@ import javax.inject.Inject;
 import javax.validation.Valid;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -35,13 +39,13 @@ import static org.elasticsearch.index.query.QueryBuilders.*;
 public class ActivoResource {
 
     private final Logger log = LoggerFactory.getLogger(ActivoResource.class);
-        
+
     @Inject
     private ActivoRepository activoRepository;
-    
+
     @Inject
     private ActivoSearchRepository activoSearchRepository;
-    
+
     /**
      * POST  /activos -> Create a new activo.
      */
@@ -90,7 +94,7 @@ public class ActivoResource {
     public ResponseEntity<List<Activo>> getAllActivos(Pageable pageable)
         throws URISyntaxException {
         log.debug("REST request to get a page of Activos");
-        Page<Activo> page = activoRepository.findAll(pageable); 
+        Page<Activo> page = activoRepository.findAll(pageable);
         HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(page, "/api/activos");
         return new ResponseEntity<>(page.getContent(), headers, HttpStatus.OK);
     }
